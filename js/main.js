a948bf2e90151042cf20faa4dbb4182b019eb330
Daniel Bugl <daniel.bugl@touchlay.com>
2013-11-29 19:27:44 +0100

    loadBranch, branch tags, working buttons, commit messages and authors

--- js/main.js
+++ js/main.js
@@ -16,22 +16,35 @@ var current_branch = "";
 
 // app
 var branches = getBranches();
+var branch_hashes = [];
 var branches_obj = $('#branches');
 for (var i=0; i < branches.length; i++) {
+    branch_hashes.push(getBranchHash(branches[i]));
     branches_obj.append(
         '<button type="button" class="btn btn-default" id="branch-' + branches[i] + '">' + branches[i] + '</button>'
     );
 }
 branches_obj.children('button').each(function () {
     $(this).click(function () {
-        selectBranch(this.innerHTML);
+        loadBranch(this.innerHTML);
     });
 });
-selectBranch("master");
+loadBranch("master");
 // /app
 
 function getBranches() {
     return fs.readdirSync(git_path + 'refs/heads/');
+}
+
+function getBranchHash(branch) {
+    var contents = fs.readFileSync(git_path + 'refs/heads/' + branch, 'utf8');
+    return contents.replace(/(\r\n|\n|\r)/gm, "");
+}
+
+function loadBranch(branch) {
+    selectBranch(branch);
+    $('#tree').empty();
+    loadCommit(branch);
 }
 
 function deselectBranch(branch) {
@@ -49,14 +62,27 @@ function selectBranch(branch) {
 }
 
 function loadCommit(hashish) {
-    selectBranch(hashish);
     repo.loadAs("commit", hashish, onCommit);
 }
 
 function onCommit(err, commit, hash) {
     if (err) throw err;
     console.log("COMMIT", hash, commit);
-    loadTree(commit.tree);
+
+    var buffer = '<li class="commit">';
+    var branch = branch_hashes.indexOf(hash);
+    console.log(branch_hashes);
+
+    if (branch != -1) buffer += '<span class="label label-primary">' + branches[branch] + '</span> ';
+
+    buffer += commit.message +
+        ' <small class="author"> - ' + commit.author.name + ' &lt;' + commit.author.email + '&gt;</small>';
+
+    buffer += '</li>';
+
+    $('#tree').append(buffer);
+
+    //loadTree(commit.tree);
     if (commit.parents) {
         commit.parents.forEach(loadCommit);
     }
